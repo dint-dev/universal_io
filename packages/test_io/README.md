9d361d30d995ce826f5a6f147a85b6e30ddd33ba
terrier989 <terrier989@gmail.com>
2020-01-08 08:56:43 +0000

    Improved documentation.

--- packages/test_io/README.md
+++ packages/test_io/README.md
@@ -1,8 +1,10 @@
 # Description
 
-This contains IO / platform related helpers for testing.
+This contains cross-platform testing helpers related to I/O and platform information.
 
 ## Getting environmental variables
+Unlike [Platform.environment](https://api.dartlang.org/stable/2.7.0/dart-io/Platform/environment.html),
+this method works in browsers too:
 ```dart
 final env = await getEnvironmentalVariables();
 print(env['EXAMPLE']);
--- packages/test_io/lib/test_io.dart
+++ packages/test_io/lib/test_io.dart
@@ -1,8 +1,8 @@
 import 'src/impl_vm.dart' if (dart.library.html) 'src/impl_browser.dart'
     as impl;
 
-/// Returns environmental variables. Unlike `Platform.environment,` works in
-/// both VM and browser.
+/// Returns environmental variables. Unlike [Platform.environment](https://api.dartlang.org/stable/2.7.0/dart-io/Platform/environment.html),
+/// this method works in browsers too.
 Future<Map<String, String>> getEnvironmentalVariables() {
   _environmentalVariables ??= impl.getEnvironmentalVariables();
   return _environmentalVariables;
