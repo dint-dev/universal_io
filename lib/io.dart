8918cd7a69ceed0e3e5d66729a29121bbf14d293
Thomas Ostfeld <thomas.ostfeld@simpleclub.com>
2024-12-13 12:04:39 +0100

    changed conditional import checks from html to js_interop

--- lib/io.dart
+++ lib/io.dart
@@ -22,5 +22,5 @@
 library universal_io;
 
 export 'src/_exports_in_vm.dart'
-    if (dart.library.html) 'src/_exports_in_browser.dart'
+    if (dart.library.js_interop) 'src/_exports_in_browser.dart'
     if (dart.library.js) 'src/_exports_in_nodejs.dart';
